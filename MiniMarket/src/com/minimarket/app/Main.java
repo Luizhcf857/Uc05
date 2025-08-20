6ecf8a242ac0a2748f5e6138557e57cbc29c25a6
Luiz Henrique <luizhenrique3781@gmail.com>
2025-08-19 21:52:12 -0300

    Foi adicionado, agora  usando heranca de classe

---
+++ MiniMarket/src/com/minimarket/app/Main.java
@@ -0,0 +1,16 @@
+package com.minimarket.app;
+
+import com.minimarket.modelos.Alimento;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("--- PRODUTO GENERICO ---");
+		Produto = produtoGenerico = new Produto("Caderno", 15.00, "12345");
+		
+
+	}
+
+}
---
+++ MiniMarket/src/com/minimarket/modelos/Produto.java
@@ -0,0 +1,5 @@
+package com.minimarket.modelos;
+
+public class Produto {
+
+}
