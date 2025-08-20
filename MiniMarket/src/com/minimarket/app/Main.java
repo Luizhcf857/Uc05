a0cf2ace21be3ff6394d534a3116b574f1ccb2ec
Luiz Henrique <luizhenrique3781@gmail.com>
2025-08-20 19:39:12 -0300

    Main 2.0
    Esse arquivo main contem o codigo finallizado e corrigido

--- MiniMarket/src/com/minimarket/app/Main.java
+++ MiniMarket/src/com/minimarket/app/Main.java
@@ -1,6 +1,8 @@
 package com.minimarket.app;
 
+import com.minimarket.modelos.Produto;
 import com.minimarket.modelos.Alimento;
+import com.minimarket.modelos.Eletronico;
 
 
 public class Main {
@@ -8,9 +10,20 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("--- PRODUTO GENERICO ---");
-		Produto = produtoGenerico = new Produto("Caderno", 15.00, "12345");
+		Produto produtoGenerico = new Produto("Caderno", 15.00, "12345");
+		produtoGenerico.exibirInformacoes();
+		System.out.println("Imposto a pagar: R$ "+ String.format("%.2f", produtoGenerico.calcularImposto()));
 		
-
+		System.out.println("\n--- PRODUTO ELETRONICO ---");
+		Eletronico smartphone = new Eletronico("Iphone X", 2500.00, "54321", 12);
+		smartphone.exibirInformacoes();
+		System.out.println("Imposto a pagar: R$ "+ String.format("%.2f", smartphone.calcularImposto()));
+		
+		System.out.println("\n--- PRODUTO ALIMENTO ---");
+		Alimento feijao = new Alimento("Feijao", 8.50, "98765", "10/12/2026");
+		feijao.exibirInformacoes();
+		System.out.println("Imposto a pagar: R$ " + String.format("%.2f", feijao.calcularImposto()));
+		
 	}
 
 }
