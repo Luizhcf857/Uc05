ad80c3eba86572135f29f904b58cfdc1a7ed9f07
Luiz Henrique <luizhenrique3781@gmail.com>
2025-09-02 21:47:48 -0300

    Add files via upload
    adicionando um projeto Teste, usando ArrayList, com um objeto do tipo Pessoa

---
+++ Teste/src/br/com/fluxo/Controller.java
@@ -0,0 +1,21 @@
+package br.com.fluxo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import br.com.modelo.Pessoa;
+
+public class Controller {
+	Scanner entrada = new Scanner(System.in);
+	ArrayList<Pessoa> pessoas = new ArrayList<>();
+	
+	public void acesso() {
+		
+		
+	}
+	
+	public void fecharScanner() {
+		entrada.close();
+		
+	}
+
+}
