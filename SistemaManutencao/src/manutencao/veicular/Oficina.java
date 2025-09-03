89d60e755d9abe10bfaad3d5adb84ce58916cf09
Luiz Henrique <luizhenrique3781@gmail.com>
2025-09-02 21:26:28 -0300

    Add files via upload
    Projeto SistemaManutencao teste, adicionado

---
+++ SistemaManutencao/src/manutencao/veicular/Oficina.java
@@ -0,0 +1,21 @@
+package manutencao.veicular;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Oficina {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		ArrayList<String> veiculosEmManutencao = new ArrayList<>();
+		
+		
+		//Variavel de controle do menu
+		int menu;
+		
+		entrada.close();
+		
+		
+	}
+
+}
